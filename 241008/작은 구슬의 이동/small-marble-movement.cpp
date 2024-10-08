0b7887a4efc1b06ddb4c84afba674786eb3d6c28
Wonbin Lee <48215558+onebeany@users.noreply.github.com>
2024-10-08 22:01:06 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241008/작은 구슬의 이동/small-marble-movement.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+// start - 21:46
+
+bool InRange(int n, int nx, int ny){
+    return (0 <= nx && nx < n && 0 <= ny && ny < n);
+}
+
+int getDir(char dir){
+    if(dir == 'E') return 0;
+    else if(dir == 'S') return 1;
+    else if(dir == 'W') return 2;
+    else return 3;
+}
+
+int main() {
+    
+    int n, t, r, c;
+    char d;
+
+    cin >> n >> t;
+    cin >> r >> c >> d;
+
+    r--, c--;
+
+    int currDir = getDir(d);
+    int dx[4]={0, 1, -1, 0}, dy[4]={1,0,0,-1};
+
+    for(int i = 0 ; i < t ; i++){
+        int nx = r + dx[currDir], ny = c + dy[currDir];
+        if(InRange(n, nx, ny)){
+            r = nx, c = ny;
+        }
+        else currDir = 3 - currDir;
+    }
+
+    cout << r+1 << " " << c+1;
+
+    return 0;
+}
