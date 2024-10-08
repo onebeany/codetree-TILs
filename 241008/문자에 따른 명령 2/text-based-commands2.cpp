7ffe301582413d5e81c3ca71eea7f0d1959c5d49
Wonbin Lee <48215558+onebeany@users.noreply.github.com>
2024-10-08 17:47:32 +0900

    Title: 문자에 따른 명령 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241008/문자에 따른 명령 2/text-based-commands2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main() {
+    int x = 0, y = 0;
+    int currDir = 3;
+    int dx[4] = {1, 0, -1 ,0}, dy[4] = {0, -1, 0, 1};
+
+    char cmd[100000];
+    cin >> cmd;
+    for(int i = 0 ; cmd[i] != '\0' ; i++){
+        if(cmd[i] == 'F')
+            x += dx[currDir], y += dy[currDir];
+        else if(cmd[i] == 'L')
+            currDir = (currDir + 3) % 4;
+        else
+            currDir = (currDir + 4) % 4; 
+    }
+    cout << x << " " << y;
+    return 0;
+}
