d4f9a8eb535d42f0c6a7b406869907a7dbb5d499
Wonbin Lee <48215558+onebeany@users.noreply.github.com>
2024-10-08 21:28:59 +0900

    Title: 방향에 맞춰 이동, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241008/방향에 맞춰 이동/move-in-direction.cpp
+++ 241008/방향에 맞춰 이동/move-in-direction.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
-#include <map>
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
-
+    
     int x = 0, y = 0;
-    map<char, int> dir;
-    dir.insert({'E', 0});
-    dir.insert({'S', 1});
-    dir.insert({'W', 2});
-    dir.insert({'N', 3});
-
-    int dx[4] = {1, 0, -1, 0}, dy[4] = {0, -1, 0, 1};
+    int dx[4] = {1,0,-1,0}, dy[4] = {0,-1,0,1};
 
     for(int i = 0 ; i < n ; i++){
-        char way;
+        char currDir;
         int dist;
-        cin >> way >> dist;
-        for(int j = 0 ; j < dist ; j++){
-            x += dx[dir[way]], y += dy[dir[way]];
-        }
-    }
+        cin >> currDir >> dist;
 
+        if(currDir == 'E') x += dx[0] * dist, y += dy[0] * dist;
+        else if(currDir == 'S') x += dx[1] * dist, y += dy[1] * dist;
+        else if(currDir == 'W') x += dx[2] * dist, y += dy[2] * dist;
+        else x += dx[3]*dist, y += dy[3]*dist;
+    }
     cout << x << " " << y;
     return 0;
 }
