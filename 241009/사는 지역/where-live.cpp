7122d3817bcea4602cfe337aeedde4c41a0bd72f
Wonbin Lee <48215558+onebeany@users.noreply.github.com>
2024-10-09 18:57:16 +0900

    Title: 사는 지역, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241009/사는 지역/where-live.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Person{
+    public:
+        string name, address, location;
+    Person(string n, string a, string l){
+        this->name = n;
+        this->address = a;
+        this->location = l;
+    }
+    Person(){}
+};
+
+int main() {
+    
+    int n;
+    cin >> n;
+    vector<Person> people(n);
+
+    int slowIdx = 0;
+    for(int i = 0 ; i < 3 ; i++){
+        string name, address, location;
+        cin >> name >> address >> location;
+        people[i] = Person(name, address, location);
+        if(people[i].name > people[slowIdx].name){
+            slowIdx = i;
+        }
+    }
+
+    cout << "name " << people[slowIdx].name << endl;
+    cout << "addr " << people[slowIdx].address << endl;
+    cout << "city " << people[slowIdx].location;
+    return 0;
+}
